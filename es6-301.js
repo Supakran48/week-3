b82dfe4a7b8d78f4c945f3020373e6c5e0e1dae7
Supakran48 <69299126+Supakran48@users.noreply.github.com>
2020-12-01 11:24:18 +0700

    Add files via upload

---
+++ es6-301.js
@@ -0,0 +1,18 @@
+//The String
+//The basic Syntax of String 
+// let message = "Hello World!";
+// console.log("Hello World!");
+//2.The basic Syntax of String
+let fname = "Mark";
+let lname = "Suckerberg";
+let fullname = fname + " " +lname;
+//console.log(fname + lname);
+//console.log(fullname);
+//3.The String with number                                                                                                                                                                                                               
+console.log("Hello") + 12; //Hello12
+console.log("10" + 2); //102
+
+console.log("10" == 10);//true
+console.log("10" == 10);//false
+console.log("10" !== 10);//
+console.log("10" <=> 10);//
